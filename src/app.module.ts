6324ee2be7b9e51d5f4c750a2d41ff42199de4f1
clarisse <clarissebleasby@gmail.com>
2026-03-17 10:49:24 -0300

    ✨ New Feature: Configurando o Banco de Dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmacia_pg2',
+      autoLoadEntities: true,
+      synchronize: true,
+      //logging: true,  - usar so pra ver o relacionamento entre as tabelas no console, nao usar em producao
+    }),],
   controllers: [],
   providers: [],
 })
